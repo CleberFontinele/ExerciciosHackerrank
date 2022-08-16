04c9d12286d72210f691263de5cb3bb9fe7d7a2b
CleberFontinele <cleberfontinele2387@gmail.com>
2022-08-16 11:35:47 -0300

    exercicio divisibleSumPairs

---
+++ 019-divisibleSumPairs.js
@@ -0,0 +1,17 @@
+function divisibleSumPairs(n, k, ar) {
+  let sum = 0;
+
+  for (let index = 0; index < n; index++) {
+    console.log(ar[index]);
+    for (let pair = 0; pair < n; pair++) {
+      if (index != pair) {
+        if ((ar[index] + ar[pair]) % k === 0) {
+          sum += 1;
+        }
+      }
+    }
+  }
+  return sum / 2;
+}
+
+console.log(divisibleSumPairs(6, 3, [1, 3, 2, 6, 1, 2]));
