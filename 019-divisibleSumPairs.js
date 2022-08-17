15cccea929600c2263c9dceed3b8dbc865b8615a
CleberFontinele <cleberfontinele2387@gmail.com>
2022-08-17 19:27:27 -0300

    exercicio countApplesAndOranges

--- 019-divisibleSumPairs.js
+++ 019-divisibleSumPairs.js
@@ -2,7 +2,6 @@ function divisibleSumPairs(n, k, ar) {
   let sum = 0;
 
   for (let index = 0; index < n; index++) {
-    console.log(ar[index]);
     for (let pair = 0; pair < n; pair++) {
       if (index != pair) {
         if ((ar[index] + ar[pair]) % k === 0) {
